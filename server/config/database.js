86055a41e1d0e323074e864cf17e00be107e7a75
PaulSteelway <lewap8@gmail.com>
2021-06-19 17:07:51 +0300

    change db settings

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const DB_URI = `mongodb+srv://AlicePasechnik:QWERTY12345@cluster0.1mwhs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const DB_URI = `mongodb://0.0.0.0:27017`;
+const DB_URI = "mongodb+srv://AlicePasechnik:Qwerty_12358@cluster0.1mwhs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const DB_URI = `mongodb://0.0.0.0:27017`;
 
 mongoose.connect(DB_URI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => console.log('Successfully connected to MongoDB'));
